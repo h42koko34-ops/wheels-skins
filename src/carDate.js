eaea872b40838911a3e3ff2e74794c9575e2ad32
h42koko34-ops <h42.koko34@gmail.com>
2026-09-09 13:16:44 +0300

    separate car database into carsdate.js

---
+++ src/carDate.js
@@ -0,0 +1,273 @@
+export const CAR_DATABASE = [
+  {
+    brand: 'Hyundai (هيونداي)',
+    models: [
+      'Accent RB (أكسنت آر بي)',
+      'Accent HCI (أكسنت إتش سي آي)',
+      'Elantra MD (إلنترا إم دي)',
+      'Elantra AD (إلنترا إيه دي)',
+      'Elantra CN7 (إلنترا سي إن 7)',
+      'Elantra HD (إلنترا إتش دي)',
+      'Tucson (توسان)',
+      'Verna (فيرنا)',
+      'I10 (آي 10)',
+      'I20 (آي 20)',
+      'Creta (كريتا)',
+      'Matrix (ماتريكس)',
+      'Bayon (بايون)'
+    ]
+  },
+  {
+    brand: 'Kia (كيا)',
+    models: [
+      'Cerato Forte (سيراتو فورتي)',
+      'Cerato K3 (سيراتو كيه 3)',
+      'Grand Cerato (جراند سيراتو)',
+      'Sportage (سبورتاج)',
+      'Rio (ريو)',
+      'Picanto (بيكانتو)',
+      'Carens (كارنز)',
+      'Xceed (إكسيد)',
+      'Sorento (سورينتو)',
+      'Pegas (بيجاس)'
+    ]
+  },
+  {
+    brand: 'Nissan (نيسان)',
+    models: [
+      'Sunny N16 (صني الشكل القديم N16)',
+      'Sunny N17 (صني الشكل الجديد N17)',
+      'Qashqai (قشقاي)',
+      'Sentra (سنترا)',
+      'Juke (جوك)',
+      'Tiida (تيدا)',
+      'X-Trail (إكس تريل)'
+    ]
+  },
+  {
+    brand: 'Toyota (تويوتا)',
+    models: [
+      'Corolla (كورولا)',
+      'Yaris (ياريس)',
+      'Fortuner (فورتشنر)',
+      'C-HR (سي إتش آر)',
+      'Belta (بيلتا)',
+      'Rumion (روميون)',
+      'Land Cruiser (لاند كروزر)',
+      'Camry (كامري)'
+    ]
+  },
+  {
+    brand: 'Mercedes-Benz (مرسيدس)',
+    models: [
+      'C180 (سي 180)',
+      'C200 (سي 200)',
+      'E200 (إي 200)',
+      'E250 (إي 250)',
+      'E300 (إي 300)',
+      'A-Class (إيه كلاس)',
+      'CLA (سي إل إيه)',
+      'GLA (جي إل إيه)',
+      'GLC (جي إل سي)',
+      'GLE (جي إل إي)',
+      'S-Class (إس كلاس)',
+      'G-Class (جي كلاس)'
+    ]
+  },
+  {
+    brand: 'BMW (بي إم دبليو)',
+    models: [
+      '316 / 318 / 320 - E46 (الفئة الثالثة E46)',
+      '316 / 318 / 320 - E90 (الفئة الثالثة E90)',
+      '318 / 320 / 328 - F30 (الفئة الثالثة F30)',
+      '320 / 330 - G20 (الفئة الثالثة G20)',
+      '520 / 523 / 525 - E60 (الفئة الخامسة E60)',
+      '520 / 528 / 535 - F10 (الفئة الخامسة F10)',
+      '520 / 530 - G30 (الفئة الخامسة G30)',
+      '1 Series (الفئة الأولى)',
+      '2 Series (الفئة الثانية)',
+      '4 Series Gran Coupe (الفئة الرابعة)',
+      'X1 (إكس 1)',
+      'X3 (إكس 3)',
+      'X4 (إكس 4)',
+      'X5 (إكس 5)',
+      'X6 (إكس 6)'
+    ]
+  },
+  {
+    brand: 'Volkswagen (فولكس فاجن)',
+    models: [
+      'Golf 5 (جولف 5)',
+      'Golf 6 (جولف 6)',
+      'Golf 7 (جولف 7)',
+      'Golf 8 (جولف 8)',
+      'Passat B6 (باسات B6)',
+      'Passat B7 (باسات B7)',
+      'Passat B8 (باسات B8)',
+      'Jetta (جيتا)',
+      'Tiguan (تيجوان)',
+      'Polo (بولو)',
+      'Arteon (أرتيون)',
+      'T-Roc (تي روك)',
+      'CC (سي سي)'
+    ]
+  },
+  {
+    brand: 'Skoda (سكودا)',
+    models: [
+      'Octavia A4 (أوكتافيا A4)',
+      'Octavia A5 (أوكتافيا A5)',
+      'Octavia A7 (أوكتافيا A7)',
+      'Octavia A8 (أوكتافيا A8)',
+      'Kodiaq (كودياك)',
+      'Superb (سوبيرب)',
+      'Karoq (كاروك)',
+      'Fabia (فابيا)',
+      'Kamiq (كاميك)',
+      'Rapid (رابد)',
+      'Scala (سكالا)'
+    ]
+  },
+  {
+    brand: 'Seat (سيات)',
+    models: [
+      'Leon (ليون)',
+      'Ibiza (إبيزا)',
+      'Ateca (أتيكا)',
+      'Tarraco (تاراكو)',
+      'Arona (أرونا)',
+      'Toledo (توليدو)'
+    ]
+  },
+  {
+    brand: 'Audi (أودي)',
+    models: [
+      'A3 (إيه 3)',
+      'A4 (إيه 4)',
+      'A5 (إيه 5)',
+      'A6 (إيه 6)',
+      'Q2 (كيو 2)',
+      'Q3 (كيو 3)',
+      'Q5 (كيو 5)',
+      'Q7 (كيو 7)'
+    ]
+  },
+  {
+    brand: 'Chevrolet (شيفروليه)',
+    models: [
+      'Optra (أوبترا)',
+      'Aveo (أفيو)',
+      'Cruze (كروز)',
+      'Lanos (لانوس)',
+      'Captiva (كابتيفا)',
+      'Sonic (سونيك)'
+    ]
+  },
+  {
+    brand: 'Chery (شيري)',
+    models: [
+      'Tiggo 3 (تيجو 3)',
+      'Tiggo 4 (تيجو 4)',
+      'Tiggo 7 (تيجو 7)',
+      'Tiggo 7 Pro (تيجو 7 برو)',
+      'Tiggo 8 (تيجو 8)',
+      'Tiggo 8 Pro (تيجو 8 برو)',
+      'Arrizo 5 (أريزو 5)',
+      'Envy (إنفي)'
+    ]
+  },
+  {
+    brand: 'MG (إم جي)',
+    models: [
+      'MG 5 (إم جي 5)',
+      'MG 6 (إم جي 6)',
+      'MG ZS (إم جي زد إس)',
+      'MG RX5 (إم جي آر إكس 5)',
+      'MG HS (إم جي إتش إس)',
+      'MG One (إم جي وان)',
+      'MG 4 (إم جي 4)'
+    ]
+  },
+  {
+    brand: 'Renault (رينو)',
+    models: [
+      'Megane (ميجان)',
+      'Logan (لوجان)',
+      'Duster (داستر)',
+      'Sandero (سانديرو)',
+      'Stepway (ستيب واي)',
+      'Kadjar (كادجار)',
+      'Fluence (فلونس)',
+      'Clio (كليو)',
+      'Austral (أوسترال)'
+    ]
+  },
+  {
+    brand: 'Peugeot (بيجو)',
+    models: [
+      '301 (بيجو 301)',
+      '208 (بيجو 208)',
+      '2008 (بيجو 2008)',
+      '308 (بيجو 308)',
+      '3008 (بيجو 3008)',
+      '508 (بيجو 508)',
+      '5008 (بيجو 5008)'
+    ]
+  },
+  {
+    brand: 'Fiat (فيات)',
+    models: [
+      'Tipo (تيبو)',
+      'Punto (بونتو)',
+      'Grande Punto (جراند بونتو)',
+      '500 (فيات 500)',
+      '500X (فيات 500 إكس)',
+      'Linea (لينيا)',
+      'Siena (سيينا)'
+    ]
+  },
+  {
+    brand: 'Mitsubishi (ميتسوبيشي)',
+    models: [
+      'Lancer بومة (لانسر بومة)',
+      'Lancer شارك (لانسر شارك)',
+      'Eclipse Cross (إكليبس كروس)',
+      'Attrage (أتراج)',
+      'Mirage (ميراج)',
+      'Outlander (أوتلاندر)',
+      'Xpander (إكسباندر)'
+    ]
+  },
+  {
+    brand: 'Honda (هوندا)',
+    models: ['Civic (سيفيك)', 'City (سيتي)', 'CR-V (سي آر في)', 'HR-V (إتش آر في)', 'Accord (أكورد)']
+  },
+  {
+    brand: 'Mazda (مازدا)',
+    models: ['Mazda 3 (مازدا 3)', 'Mazda 2 (مازدا 2)', 'Mazda 6 (مازدا 6)', 'CX-3 (سي إكس 3)', 'CX-5 (سي إكس 5)']
+  },
+  {
+    brand: 'Opel (أوبل)',
+    models: ['Astra (أسترا)', 'Insignia (إنسيجنيا)', 'Corsa (كورسا)', 'Crossland (كروس لاند)', 'Grandland (جراند لاند)', 'Mokka (موكا)', 'Vectra (فيكترا)']
+  },
+  {
+    brand: 'Geely (جيلي)',
+    models: ['Coolray (كول راي)', 'Emgrand (إمجراند)', 'Okavango (أوكافانجو)', 'Geometry C (جيومتري سي)', 'GX3 Pro (جي إكس 3 برو)', 'Starray (ستار راي)']
+  },
+  {
+    brand: 'BYD (بي واي دي)',
+    models: ['F3 (إف 3)', 'L3 (إل 3)', 'Song Plus (سونج بلس)', 'Atto 3 (أتو 3)', 'Seal (سيل)']
+  },
+  {
+    brand: 'Jeep (جيب)',
+    models: ['Grand Cherokee (جراند شيروكي)', 'Wrangler (رانجلر)', 'Renegade (رينيجيد)', 'Compass (كومباس)']
+  },
+  {
+    brand: 'Ford (فورد)',
+    models: ['Focus (فوكس)', 'Fiesta (فييستا)', 'EcoSport (إيكوسبورت)', 'Kuga (كوجا)', 'Fusion (فيوجن)']
+  },
+  {
+    brand: 'Suzuki (سوزوكي)',
+    models: ['Swift (سويفت)', 'Dzire (ديزاير)', 'Ciaz (سياز)', 'Baleno (بالينو)', 'Vitara (فيتارا)', 'Jimny (جيمني)', 'Ertiga (أرتيجا)', 'Alto (ألتو)', 'Fronx (فرونكس)']
+  }
+];
